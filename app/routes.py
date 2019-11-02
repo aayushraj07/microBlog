6d0eacba599ed6ed08c9f1a4a57bfb49dd264256
aayushraj07 <aayush.97raj@gmail.com>
2019-11-02 20:43:09 +0530

    flask-wtf installed and configured , user form created , error in user form

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,10 @@
-from app import app
 from flask import render_template
+from app import app
+from app.forms import LoginForm
 
+# ...
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Aayush'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The average movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
